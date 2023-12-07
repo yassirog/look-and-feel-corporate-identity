4eecb4764af2a307e47a031cb3a5c5b6a152a124
yassir bghiel <yassir.bghiel@hva.nl>
2023-12-07 23:36:41 +0100

    kaas

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var navButton = document.getElementById('navButton');
-
-    if (navButton) {
-        alert('Knop gevonden!');
-        navButton.addEventListener('click', function() {
-            alert('Knop geklikt!');
-            document.body.classList.toggle('blue');
-        });
-    }
+    alert('Hallo wereld!');
 });
