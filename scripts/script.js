2214025949de97a6fcc1212fef5b28575b1b2acc
yassir bghiel <yassir.bghiel@hva.nl>
2023-12-09 22:17:11 +0100

    gefixt met darioes

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById("navButton").addEventListener("click", function() {
-        // Zoek alle elementen met de class "textcontainer"
-        var sections = document.querySelectorAll("section");
+      var sections = document.querySelectorAll("section");
 
-        // Toggle de classes voor elke section
-        sections.forEach(function(section) {
-          section.classList.toggle("darktextcontainer");
-          section.classList.toggle("textcontainer")
-        });
+      sections.forEach(function(section) {
+        section.classList.toggle("darktextcontainer");
+        section.classList.toggle("textcontainer");
       });
     });
+  });
